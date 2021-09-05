9c97bce2ff9dabe212423c3090bd17c2f74723c4
Vikas <vikassuthar528@gmail.com>
2021-09-05 12:36:07 +0530

    added basic structure of home page

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./css/Navbar.css";
+
+const Navbar = (props) => {
+    return (
+        <navbar>
+        <h2>This is navbar</h2>
+        </navbar>
+    );
+};
+
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,27 @@ import React, { useEffect } from "react";
 
 import "./css/Home.css";
 
+// components import
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 const Home = (props) => {
   useEffect(() => {
     document.title = props.title;
   }, [props.title]);
   return (
     <main className="Home">
-      <h1>Landing Page of Aashroy</h1>
+      < Navbar />
+      <h2 id="carousel">carousel</h2>
+      <h2 id="home-map">Map</h2>
+      <div id="upload-img">
+        <img src="img/photo.png" id="upload-png"></img>
+        <h2>Upload Here</h2>
+      </div>
+      < Footer />
     </main>
   );
 };
 
+// <div className="break-line"></div>
 export default Home;
