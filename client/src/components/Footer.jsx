ce90a04e24b8ed11c380e698b6b986eed31e7c0d
Vikas <vikassuthar528@gmail.com>
2021-09-08 00:15:57 +0530

    completed navbar and fixed sayan's carousel

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,20 @@ import "./css/Footer.css";
 
 const Footer = (props) => {
     return (
-        <footer></footer>
+        <footer>
+            <p id="contact-us">Contact Us</p>
+            <div id="github-link">
+                <a href="https://github.com/decoding-apocalypse/aashroy">
+                    <img src="./img/GitHub-Mark-Light-32px.png"></img>
+                    <span>Github</span>
+                </a>
+            </div>
+            <div id="copyright-info">
+                <span> Copyright © 2018 </span> | 
+                <span> Terms Of Use </span> | 
+                <span> Privacy Policy </span>
+            </div>
+        </footer>
     );
 };
 
