139a09f888d13f044f98fcf57db37df9220cd75b
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-09-16 23:55:07 +0530

    fix: Report page responsive

--- client/src/pages/Report.jsx
+++ client/src/pages/Report.jsx
@@ -16,49 +16,50 @@ const Report = (props) => {
       <div className="report-content">
         <h1>Online Complaint Portal</h1>
       </div>
-      <div className="report-main">
-        <p>
-          The only place where your complaints are precious to us! If you have a
-          complaint or want to report a crime or any illegal atrocities against
-          the helpless, please feel free to report through an anonymous
-          reporting mechanism where we try to help and build the society through
-          your hands. <br />
-          You can also share your valuable feedback to us which can help us to
-          serve the community better.
-        </p>
-        <button type="button" className="btn btn-outline-info">
-          Complaint Now
-        </button>
-      </div>
-      <div>
-        <img className="report-bg" src="/img/report-bg-1.png" alt="nothing" />
+      <div className="reportContainer">
+        <div className="report-main">
+          <p>
+            The only place where your complaints are precious to us! If you have
+            a complaint or want to report a crime or any illegal atrocities
+            against the helpless, please feel free to report through an
+            anonymous reporting mechanism where we try to help and build the
+            society through your hands. <br />
+            You can also share your valuable feedback to us which can help us to
+            serve the community better.
+          </p>
+          <button type="button" className="btn btn-outline-info">
+            Complaint Now
+          </button>
+        </div>
+        <div className="report-bg">
+          <img src="/img/report-bg-1.png" alt="nothing" />
+        </div>
       </div>
 
       <div className="report-form">
         <h3 className="form-heading">Feedback Form for Aakansha</h3>
         <form>
-          <label htmlFor="fname">Your Name</label>
           <input
             type="text"
             className="fname"
             name="firstname"
-            placeholder="Your name.."
+            placeholder="Name"
+            required
           />
 
-          <label htmlFor="email">Email</label>
           <input
             type="text"
             className="fname"
             name="email"
-            placeholder="Your Email Id here"
+            placeholder="E-Mail"
+            required
           />
 
-          <label htmlFor="lname">Feedback</label>
           <input
             type="text"
             className="fname"
             name="lastname"
-            placeholder="Your valuable feedback.."
+            placeholder="Please give your valuable feedback"
           />
 
           <input className="submit" type="submit" value="Submit" />
