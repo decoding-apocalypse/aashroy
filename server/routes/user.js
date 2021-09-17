87e9432de48d806a4df25641e7f285d25c2310d8
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-09-17 14:18:24 +0530

    feat: more user route add

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Register route
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
