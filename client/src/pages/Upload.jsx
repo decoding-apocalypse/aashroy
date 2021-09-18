ba14aab0cb06d4691040d0489825618c4e432d2e
Vikas <vikassuthar528@gmail.com>
2021-09-18 21:53:12 +0530

    added map and upload file functioning and styled the upload button

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+// import { Image } from "cloudinary-react";
 
 import Map from "../components/Map";
 
 import "./css/Upload.css";
+
+const UploadDefaultState = {
+  location: { lat: null, lng: null },
+  imgUrl: "",
+  descrip: "",
+};
 
 const Upload = (props) => {
   useEffect(() => {
@@ -23,14 +30,67 @@ const Upload = (props) => {
     });
   }, []);
 
+  const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`;
+  const [imageSelected, setImageSelected] = useState([]);
+  const [description, setDescripSelected] = useState();
+  const [uploadFiles, setUploadedFiles] = useState([]);
+  const [uploadData, setUploadData] = useState(UploadDefaultState);
+
+  console.log(uploadData);
+
+  const handelingMapData = (lat, lng) => {
+    setUploadData((previousData) => ({
+      ...previousData,
+      location: { lat, lng },
+    }));
+  };
+  const imageSelectedHandler = () => {
+    setImageSelected(async (imageSelect) => {
+      const formData = new FormData();
+      formData.append("file", imageSelect);
+      formData.append("upload_preset", process.env.REACT_APP_UPLOAD_PRESET);
+
+      const res = await fetch(url, {
+        method: "post",
+        body: formData,
+      });
+      const resData = await res.json();
+      // console.log(resData.url);
+      setUploadData((previousData) => ({
+        ...previousData,
+        imgUrl: resData.url,
+      }))
+      setUploadedFiles((old) => [...old, resData]);
+    });
+  };
+
+  const descripSelectedHandler = () => {
+    setDescripSelected((event) => {
+      console.log(event);
+    })
+  }
+
   return (
     <main>
       <h2>Please Select Location</h2>
-      <div id="location-map">{location && <Map location={location} />}</div>
+      <div id="location-map">{location && <Map location={location} onMapData={handelingMapData}/>}</div>
       <div id="upload-img">
         <div id="upload-btn">
-          <button>
-            <img src="img/photo.png" alt="Upload"></img> Upload
+          <input
+            id="file-type-inp"
+            type="file"
+            accept="image/*"
+            onChange={(event) => {
+              setImageSelected(event.target.files[0]);
+            }}
+          />
+          <label for="file-type-inp" id="file-type-inp-label">Choose a file</label>
+          <input id="text-type-inp" type="text" placeholder="description..." onChange={(event) => {
+            setDescripSelected(event);
+          }}/>
+          <button onClick={imageSelectedHandler}>
+            <img id="upload-img-btn" src="img/photo.png" alt="Upload"></img>{" "}
+            Upload
           </button>
         </div>
       </div>
