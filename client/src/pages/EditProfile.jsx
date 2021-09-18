8771163a412fb0f38b6f3f3c11683bfe1f4651cf
Sayahnneeta <sayahnneetadutta@gmail.com>
2021-09-19 01:58:49 +0530

    edit profile made responsive

---
+++ client/src/pages/EditProfile.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+
+import "./css/EditProfile.css";
+
+const EditProfile = (props) => {
+  useEffect(() => {
+    document.title = props.title;
+  }, [props.title]);
+  return (
+      <main className="EditProfile">
+          <h1 className="edit-heading">Edit Profile</h1>
+        <div className="edit-content">
+           <img src="/img/edit-user.jpg" alt="" srcset="" />
+        </div>
+           <div className="user-img">
+           <a href="#">Change Profile Picture</a>
+           </div>
+        <div className="edit-form">
+        <h3 className="form-heading">Edit your Details</h3>
+        <form>
+          <input
+            type="text"
+            className="fname"
+            name="firstname"
+            placeholder="Name"
+            required
+          />
+
+          <input
+            type="text"
+            className="fname"
+            name="email"
+            placeholder="E-Mail"
+            required
+          />
+
+          <input
+            type="text"
+            className="fname"
+            name="lastname"
+            placeholder="Update your bio"
+          />
+
+          <input className="submit" type="submit" value="Submit" />
+        </form>
+        </div>   
+      </main>
+  );
+}
+export default EditProfile;
